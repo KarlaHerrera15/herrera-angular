e95006ae2f58102ec0722ee3bf4160a3ecf60e94
KarlaHerrera15 <herrera.karla@iisgalvanimi.edu.it>
2024-11-28 09:04:47 +0000

    punto 3

--- herrera/src/app/app-component2/app-component2.component.ts
+++ herrera/src/app/app-component2/app-component2.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { AppComponent3Component } from '../app-component3/app-component3.component';
 
 @Component({
   selector: 'app-app-component2',
-  imports: [],
+  imports: [AppComponent3Component],
   templateUrl: './app-component2.component.html',
   styleUrl: './app-component2.component.css'
 })
